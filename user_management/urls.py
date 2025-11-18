ba0d96eac22a79d1426eb2d9fc296c7286e222c6
gautambafnaFM <gautam.bafna@flairminds.com>
2025-11-18 23:33:20 +0530

    mgeation done

---
+++ user_management/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Define your user management routes here, e.g.:
+    # path('users/', views.user_list, name='user-list'),
+]
